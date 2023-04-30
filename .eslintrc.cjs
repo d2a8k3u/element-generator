3e47d2cf4ab290914e9a9fb16c4811e1b022110b
David Kulhánek <davidkulh182@gmail.com>
2023-04-30 17:47:59 +0200

    Base config settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,41 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    node: true,
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'prettier'
   ],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    yoda: [
+      'warn',
+      'always',
+      {
+        exceptRange: true,
+        onlyEquality: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        arrowParens: 'always',
+        tabWidth: 2,
+        semi: true,
+      },
+    ],
+  },
 }
